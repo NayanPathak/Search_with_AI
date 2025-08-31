e1998e0c961127279619f4f688346afa63565a22
Nayan Pathak <pathaknayan8582@gmail.com>
2025-09-01 04:21:54 +0530

    Integrat Gemini API

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,33 @@
 import { useState } from "react";
+import { URL } from "./constant";
 
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [question, setQuestion] = useState("");
+
+  const [Answer, setAnswer] = useState(undefined);
+  const payload = {
+    contents: [
+      {
+        parts: [
+          {
+            text: question,
+          },
+        ],
+      },
+    ],
+  };
+
+  const askquestion = async () => {
+    let response = await fetch(URL, {
+      method: "POST",
+      body: JSON.stringify(payload),
+    });
+    response = await response.json();
+    //  console.log(response.candidates[0].content.parts[0].text);
+    setAnswer(response.candidates[0].content.parts[0].text);
+  };
 
   return (
     <>
@@ -11,9 +35,13 @@ function App() {
         <div className="col-span-1 w-2/3 bg-gradient-to-b from-yellow-500 to-yellow-700 "></div>
         <div className="col-span-4">
           <h1 className="text-4xl font-bold text-white mt-20 mb-10">
-            hello user
+            <span class="bg-gradient-to-r from-blue-500 to-yellow-700 text-transparent bg-clip-text">
+              hello user !!!
+            </span>
           </h1>
-          <div className="container h-150"></div>
+          <div className="container h-140 overflow-auto mb-10 p-5">
+            <div className="text-white">{Answer}</div>
+          </div>
 
           <div
             className="border border-white w-1/2 rounded-4xl
@@ -21,11 +49,18 @@ function App() {
           >
             <input
               type="text"
+              value={question}
+              onChange={(e) => setQuestion(e.target.value)}
               className=" p-3 outline-none  w-full h-full
                 text-white placeholder:text-zinc-400"
               placeholder="Search anything...."
             />
-            <button className="text-yellow-500 font-bold px-4 py-2">Ask</button>
+            <button
+              onClick={askquestion}
+              className="text-yellow-500 font-bold px-4 py-2"
+            >
+              Ask
+            </button>
           </div>
         </div>
       </div>
